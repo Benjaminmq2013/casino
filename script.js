f7ba24e47c49b7cc3d5efd830c089c61365d4599
Adriel Benjamín Martínez Quintanilla <adriel.martinez@jdoutstanding.com>
2025-02-15 22:01:29 -0600

    Second Version: Randomize!

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 const rollBtn = document.querySelector(".roll-btn")
-const result = document.querySelector(".result")
+const resultBox = document.querySelector(".result")
 rollBtn.addEventListener("click", letsRoll)
 
 function letsRoll () {
-    console.log(result)
-    result.innerHTML = "Ganaste!"
+    var result = "Pierdes!"
+
+    const random = Math.random() * (10 - 1) + 1;
+    
+
+    if(random >= 5 ) result = "Ganaste!"
+
+    calculateNewMoney(result)
+    setNewMoney()
+
+    
+    resultBox.innerHTML = `${result}`
 }
+
+
+const moneyBox = document.querySelector(".money-box")
+var currentMoney = 5;
+moneyBox.innerHTML = currentMoney;
+
+function calculateNewMoney (result) {
+    if(result === "Ganaste!") {
+        currentMoney += 0.5;
+    } else {
+        currentMoney -= 0.5;
+    }
+}
+
+const setNewMoney = () => moneyBox.innerHTML = `${currentMoney}`;
+setNewMoney()
