07fa20da14c304325f7083f9533c729eebba2d4a
Adriel Benjamín Martínez Quintanilla <adriel.martinez@jdoutstanding.com>
2025-02-23 00:58:06 -0600

    Save: Redesign completed

---
+++ dist/ts/UI.js
@@ -0,0 +1,15 @@
+"use strict";
+const cardsTemplate = () => {
+    let template = "";
+    for (let i = 0; i < 11; i++) {
+        template = template +
+            `<div class="card"> <img class="slot__image" src="/public/assets/artworks/image-item-${i + 1}.png" /> </div>`;
+    }
+    return template;
+};
+document.addEventListener("DOMContentLoaded", () => {
+    const cardScrollCollection = document.querySelectorAll(".card__scroll");
+    cardScrollCollection.forEach((cardScroll, index) => {
+        cardScroll.innerHTML = cardsTemplate();
+    });
+});
